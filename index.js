1a31a908ff21147176a5234c2ab39967137fb510
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-05-16 13:26:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+displayMessage(){
+  
+}
+if (timeString < 12) {
+  h1.value = "Good morning";
+} else if (timeString > 12, timeString <17) {
+  h1.value = "Good day";
+} else {
+  h1.value = "Good evening";
+}
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
